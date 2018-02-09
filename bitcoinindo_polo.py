c8e55eb732fccd6a8fc6fbb3a56b4f0828535a4a
medreza <31310263+medreza@users.noreply.github.com>
2018-02-09 08:13:24 +0700

    Add bitcoin indo and poloniex market diff

---
+++ bitcoinindo_polo.py
@@ -0,0 +1,30 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+alamat = "https://coinmarketcap.com/exchanges/bitcoin-indonesia/"
+page = urllib2.urlopen(alamat)
+soup = BeautifulSoup(page, "html.parser")
+a_tabel = soup.find_all('table')[0].find('a', string="BTC/IDR").parent.parent
+btcindo = float(a_tabel.find("span", {"class":"price"})['data-native'])
+
+alamat = "https://coinmarketcap.com/exchanges/poloniex/"
+page = urllib2.urlopen(alamat)
+soup = BeautifulSoup(page, "html.parser")
+a_tabel = soup.find_all('table')[0].find('a', string="BTC/USDT").parent.parent
+btcpolo_usdt = float(a_tabel.find("span", {"class":"price"})['data-native'])
+
+alamat = "https://coinmarketcap.com/exchanges/poloniex/"
+page = urllib2.urlopen(alamat)
+soup = BeautifulSoup(page, "html.parser")
+currencyrate = round(float(soup.find(id="currency-exchange-rates")['data-idr']), 8)
+
+btcpolo = btcpolo_usdt/currencyrate
+
+print "Harga 1 BTC dalam rupiah"
+print "Bitcoin Indo :"+str(btcindo)
+print "Poloniex     :"+str(btcpolo)
+selisih = ((btcindo-btcpolo)/btcpolo)*100
+print "Selisih      :"+str(selisih)+" %"
+print ""
+print "Withdrawal Fee Bitcoin Indo (0.0005 BTC): Rp" + str(btcindo*0.0005)
+print "Withdrawal Fee Poloniex     (0.001 BTC) : Rp" + str(btcpolo*0.001)
